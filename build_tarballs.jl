dcf92cf49c1ef870ad88c8bf64c6ff1e3d39be96
UnofficialJuliaMirrorBot <43731649+UnofficialJuliaMirrorBot@users.noreply.github.com>
2019-07-18 18:27:39 +0000

    Snapshot of branch master taken on 2019-07-18T14:27:38.226-04:00 from "https://github.com/UnofficialJuliaMirror/symengine-SymEngineBuilder"

--- build_tarballs.jl
+++ build_tarballs.jl
@@ -17,14 +17,17 @@ script = """cd symengine-$version
 if [[ ${target} == x86_64-* ]]; then
     export CXXFLAGS=-march=core2
 fi
-cmake -DCMAKE_INSTALL_PREFIX=$prefix -DCMAKE_TOOLCHAIN_FILE=/opt/$target/$target.toolchain -DBUILD_TESTS=no -DBUILD_BENCHMARKS=no  -DBUILD_SHARED_LIBS=yes -DWITH_MPC=yes -DBUILD_FOR_DISTRIBUTION=yes .
+cmake -DCMAKE_INSTALL_PREFIX=$prefix -DCMAKE_TOOLCHAIN_FILE=/opt/$target/$target.toolchain -DBUILD_TESTS=no -DBUILD_BENCHMARKS=no  -DBUILD_SHARED_LIBS=yes -DWITH_MPC=yes -DBUILD_FOR_DISTRIBUTION=yes -DWITH_SYMENGINE_THREAD_SAFE=yes .
 make -j
 make install
 """
 
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
-platforms = supported_platforms()
+a = supported_platforms()
+deleteat!(a, findfirst(x->x==Windows(:x86_64), a))
+platforms = BinaryBuilder.expand_gcc_versions(Windows(:x86_64))
+platforms = vcat(platforms, a)
 
 # The products that we will ensure are always built
 products(prefix) = [
@@ -33,9 +36,9 @@ products(prefix) = [
 
 # Dependencies that must be installed before this package can be built
 dependencies = [
-    "https://github.com/JuliaMath/GMPBuilder/releases/download/v6.1.2-2/build_GMP.v6.1.2.jl"
-    "https://github.com/JuliaMath/MPFRBuilder/releases/download/v4.0.1-3/build_MPFR.v4.0.1.jl"
-    "https://github.com/isuruf/MPCBuilder/releases/download/v1.1.0-2/build_MPC.v1.1.0.jl"
+    "https://github.com/JuliaPackaging/Yggdrasil/releases/download/GMP-v6.1.2-1/build_GMP.v6.1.2.jl"
+    "https://github.com/JuliaPackaging/Yggdrasil/releases/download/MPFR-v4.0.2-0/build_MPFR.v4.0.2.jl"
+    "https://github.com/isuruf/MPCBuilder/releases/download/v1.1.0-3/build_MPC.v1.1.0.jl"
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
